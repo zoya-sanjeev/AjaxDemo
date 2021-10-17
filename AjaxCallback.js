cdecb638c7b70ce6202bd90dd2d3979476ceefab
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-17 19:29:38 +0530

    ADDED: function to add details using post method

--- AjaxCallback.js
+++ AjaxCallback.js
@@ -17,20 +17,33 @@ function makeAJAXCall(methodType, url, callback, async = true, data= null){
         }
     }
     xhr.open(methodType,url,async);
-    xhr.send();
+    if(data){
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    }else
+        xhr.send();
     console.log(methodType+" request sent to server at:"+ showTime());
 }
 
-const getURL ="http://127.0.0.1:3000/employees/";
+const getURL ="http://127.0.0.1:3000/employees/1";
 function getUserDetails(data){
     console.log("Get user data at:"+ showTime() + " data:" + data);
 }
 
 makeAJAXCall("GET", getURL, getUserDetails, true);
-console.log("Made Get AJAX Call to Server at:"+ showTime());
+console.log("Made GET AJAX Call to Server at:"+ showTime());
 
 const deleteURL = "http://127.0.0.1:3000/employees/4";
 function userDeleted(data){
     console.log("User deleted" + data);
 }
 makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+console.log("Made DELETE AJAX Call to Server at:"+ showTime());
+
+const postURL = "http://127.0.0.1:3000/employees";
+const employeeData = {"name": "Harry", "salary":"5000"};
+function userAdded(data){
+    console.log("User Added:" + data);
+}
+makeAJAXCall("POST", postURL, userAdded, true, employeeData);
+console.log("Made POST AJAX Call to Server at:"+ showTime());
